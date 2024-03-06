2163541728da56eef49b7c8ecdde3f2f41e38b6a
nathaniel nosa <nathanielnarth@gmail.com>
2024-03-06 11:45:35 +0100

    Setting up the index file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-M-V-C
+require('dotenv').config()
+const express = require('express')
+
+
+// initialize an express object
+const app = express()
+
+// for json data
+app.use(express.json())
+
+//port
+const PORT = process.env.PORT || 5890
+
+//connect
+app.listen(PORT, () => {
+  console.log(`server is running on port http://localhost:${PORT}`)
+})

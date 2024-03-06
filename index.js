0fc2c10ed904445192eecdacb938ba511f30d12b
nathaniel nosa <nathanielnarth@gmail.com>
2024-03-06 12:26:06 +0100

    mongodb setup

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 require('dotenv').config()
 const express = require('express')
-
+const connectDB = require('./config/dbConfig')
+const mongoose = require('mongoose')
 
 // initialize an express object
 const app = express()
 
 // for json data
 app.use(express.json())
+//db config
+connectDB()
 
 //port
 const PORT = process.env.PORT || 5890
 
 //connect
-app.listen(PORT, () => {
-  console.log(`server is running on port http://localhost:${PORT}`)
+mongoose.connection.once('open', () => {
+  console.log('Database connection success!')
+
+  app.listen(PORT, () => {
+    console.log(`server is running on port http://localhost:${PORT}`)
+  })
 })
